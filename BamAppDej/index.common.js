d78bd1411713961e2bb47890a9695e01b8295102
Florian Rival <Florian.Rival@gmail.com>
2015-10-31 12:23:26 +0100

    Merge common code in index.common.js

--- BamAppDej/index.common.js
+++ BamAppDej/index.common.js
@@ -1,1 +1,62 @@
-//TODO
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var Button = require('react-native-button');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var BamAppDej = React.createClass({
+
+  _goCreateOption: function() {
+    console.log("HELLO WORLD")
+  },
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <Button style={{color: 'green'}} onPress={this._goCreateOption}>
+          Press Me!
+        </Button>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+module.exports = BamAppDej;
