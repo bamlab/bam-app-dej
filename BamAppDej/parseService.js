8e47e9f4c26e6170cd0c2545184bc45ea1632835
Florian Rival <Florian.Rival@gmail.com>
2015-10-31 15:08:53 +0100

    Remove dead code

--- BamAppDej/parseService.js
+++ BamAppDej/parseService.js
@@ -3,10 +3,6 @@ var LunchOption = Parse.Object.extend("LunchOption");
 
 function ParseService() {
 	Parse.initialize("FDag4VvDooHzkS6joKK845O8gDWNpwnicK57CxLv", "P4ByaCIjUmszYBK5CNVbTypOK2m1X2zQMQKjpPVB");
-
-	var TestObject = Parse.Object.extend("TestObject");
-	var testObject = new TestObject();
-	testObject.save({foo: "bar"});
 }
 
 ParseService.prototype.loadAllLunchOptions = function() {
